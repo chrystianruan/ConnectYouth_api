ed6daf2564588174ea9a78537cddd891a7bd318e
Chrystian Ruan <chrystianr37@gmail.com>
2023-08-02 19:30:36 -0300

    continuidade do projeto

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -2,24 +2,43 @@
 
 namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function authentication(Request $request) {
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'admin' => 1])) {
-            return redirect('/admin/dashboard');
-        }
-
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'master' => 1])) {
-            return redirect('/master/dashboard');
-        }
-
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'master' => 0, 'admin' => 0])) {
-            return redirect('/dashboard');
+            $user = Auth::user();
+            $token = $user->createToken("Token");
+            return response()->json([
+                'response' => 'Usuário logado com sucesso',
+                'token' => $token->plainTextToken,
+                'admin' => false,
+                'master' => false,
+                'para_moises' => 'você é um otário'
+            ], 200);
         }
 
-        return redirect()->back()->with('message', 'Seu usuário ou senha estão errados');
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'admin' => 1, 'master' => 0])) {
+            $user = Auth::user();
+            $token = $user->createToken("Token");
+            return response()->json([
+                'response' => 'Usuário logado com sucesso',
+                'admin' => true,
+                'master' => false,
+                'token' => $token->plainTextToken,
+                'para_moises' => 'você é um otário'
+            ], 200);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'master' => 1, 'admin' => 0])) {
+
+        }
+
+        return response()->json([
+            'response' => 'Erro ao logar'
+        ], 405);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\api\UserController;
 use \App\Http\Controllers\api\EventController;
+use \App\Http\Controllers\api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,8 +23,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::post('/new-user', [UserController::class, 'newSimpleUser']);
+Route::post('/new-admin-user', [UserController::class, 'newAdminUser']);
 Route::get('/users', [UserController::class, 'getUsers']);
 Route::get('/user/{id}', [UserController::class, 'getPersonalInfosToUser']);
 Route::get('/user/{id}', [UserController::class, 'getPersonalInfosToUser']);
 Route::post('/new-event', [EventController::class, 'newEvent']);
 Route::get('/events', [EventController::class, 'getAllEvents']);
+Route::post('/logon', [AuthController::class, 'authentication']);
