cb73728016bcdf724e4ececddf81a15b976f27e4
chrystianruan <chrystianr37@gmail.com>
2024-01-31 23:39:54 -0300

    fix: corrigindo commit anterior. Trocando public por storage

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Storage;
 class EventService {
 
     public function newEvent($request) {
-        $image = Storage::put('public/images', $request->image);
+        $image = Storage::put('storage/images', $request->image);
 
         $authUser = User::leftJoin('personal_informations_users', 'personal_informations_users.user_id', '=', 'users.id')
             ->findOrFail(auth()->user()->id);
